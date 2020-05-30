cce2837bff458181a918693a9ab77e5fbd749c04
munsuyog <36353964+munsuyog@users.noreply.github.com>
2020-05-30 13:34:25 +0530

    Init : Switch to device specific libinit
    Also start using blueprint

--- init/Android.mk
+++ init/Android.mk
@@ -1,18 +1,31 @@
-LOCAL_PATH := $(call my-dir)
+//
+// Copyright (C) 2019 The LineageOS Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-LOCAL_C_INCLUDES := \
-    system/core/base/include \
-    system/core/init \
-    external/selinux/libselinux/include
-LOCAL_CFLAGS := -Wall
-LOCAL_SRC_FILES := init_msm8916.cpp
-ifneq ($(TARGET_LIBINIT_MSM8916_DEFINES_FILE),)
-  LOCAL_SRC_FILES += ../../../../$(TARGET_LIBINIT_MSM8916_DEFINES_FILE)
-endif
-LOCAL_MODULE := libinit_msm8916
-LOCAL_STATIC_LIBRARIES := libbase libselinux
-
-include $(BUILD_STATIC_LIBRARY)
+cc_library_static {
+    name: "libinit_passion",
+    recovery_available: true,
+    srcs: ["init_passion.cpp"],
+    include_dirs: [
+        "system/core/base/include",
+        "system/core/init",
+        "external/selinux/libselinux/include",
+    ],
+    static_libs: [
+        "libbase",
+        "libselinux",
+    ],
+    cflags: ["-Wall"]
+}
